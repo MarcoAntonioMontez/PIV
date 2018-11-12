373a962881fdf2294c0602b9b31eff0bd5f93aa7
TheBeggar <hugo_piku@hotmail.com>
2018-11-12 19:43:06 +0000

    main2 lines

--- main2.m
+++ main2.m
@@ -13,14 +13,19 @@ im2=rgb2gray(imread('test_2/rgb_image2_0052.png'));
 % d1 = load('test_2/depth1_0052.mat');
 % d2 = load('test_2/depth1_0052.mat');
 %%
-[f1, d1] = v1_sift(single(im1));
-[f2, d2] = v1_sift(single(im2));
+[f1, d1] = vl_sift(single(im1));
+[f2, d2] = vl_sift(single(im2));
 figure(1);
 imagesc(im1); hold on; plot(f1(1,:), f1(2,:), '*'); hold off;
 figure(2);
 imagesc(im2); hold on; plot(f1(1,:), f1(2,:), '*'); hold off;
 %%
-[match, sc] = vl_ubmatch(d1, d2);
+[match, sc] = vl_ubcmatch(d1, d2);
 figure(3);
 imagesc([im1, im2]);
 hold on;
+% [minValue_x, maxValue_x, minValue_y, maxValue_y, minValue_z, maxValue_z] = boxplot( class_x, class_y, class_depth); 
+line([minValue_y maxValue_y],[minValue_x minValue_x],'Color','red')
+line([minValue_y maxValue_y],[maxValue_x maxValue_x],'Color','red')
+line([minValue_y minValue_y],[minValue_x maxValue_x],'Color','red')
+line([maxValue_y maxValue_y],[minValue_x maxValue_x],'Color','red')
