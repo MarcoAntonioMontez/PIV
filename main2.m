3ebd42275a384b6e1d6cb9a7bdf6f4f23c5697b3
joaoalves <jmalves5@hotmail.com>
2018-12-05 15:21:45 +0000

    supposed to work. i doubt it works

--- main2.m
+++ main2.m
@@ -1,6 +1,6 @@
-%run('vlfeat-0.9.21/toolbox/vl_setup')
-%vl_version verbose
-close all; clear; clc;
+% run('vlfeat-0.9.21/toolbox/vl_setup')
+% vl_version verbose
+close all; clear;
 format compact
 
 im1=rgb2gray(imread('fruta2/rgb_image1_0011.png'));
@@ -18,10 +18,13 @@ im2d = load('fruta2/depth2_0011.mat');
 %                 S is the scale and TH is the orientation (in radians).
 % d = 128-dimensional vector of class UINT8.
 
-%Values to calculate centroids
-length_f1=size(f1,2);
-length_f2=size(f2,2);
+%get all feature points
+length_f1=length(f1);
+length_f2=length(f2);
 
+
+
+%Values to calculate centroids
 avf1x=sum(f1(1,:))/length_f1;
 avf1y=sum(f1(2,:))/length_f1;
 avf2x=sum(f2(1,:))/length_f2;
@@ -29,8 +32,8 @@ avf2y=sum(f2(2,:))/length_f2;
 
 acum1=double(0);
 for i = 1:length_f1
-    x=fix(f1(1,i));
-    y=fix(f1(2,i));
+    x=round(f1(1,i));
+    y=round(f1(2,i));
     
     acum1=acum1+double(im1d.depth_array(y,x));
 end
@@ -84,90 +87,131 @@ axis image off ;
 %getting x,y points from matches --> from f1, f2
 %account for matlab switching stuff
 
-%1st random pair
-xyzpair1 = zeros(3,2);
+Rsave=zeros(3,3,30);
+Tsave=zeros(3,1,30);
+inlierssave=zeros(1,30);
 
-while(ismember(0, xyzpair1))
-    random1 = round(rand*length(match));
-    if random1==0
-        random1=1;
+for k=1:60
+    %1st random pair
+    xyzpair1 = zeros(3,2);
+
+    while(ismember(0, xyzpair1))
+        random1 = round(rand*length(match));
+        if random1==0
+            random1=1;
+        end
+        pair1 = match(:, random1);
+        f1temp = f1(:,pair1(1));
+        f2temp = f2(:,pair1(2));
+        xypair1 = [round(f2temp(1:2)), round(f1temp(1:2))];  %dealt with matlab shit
+        xyzpair1 = vertcat(xypair1, horzcat(im1d.depth_array( xypair1(2,1), xypair1(1,1)), im2d.depth_array(xypair1(2,2), xypair1(1,2))));
     end
-    pair1 = match(:, random1);
-    f1temp = f1(:,pair1(1));
-    f2temp = f2(:,pair1(2));
-    xypair1 = [round(f2temp(1:2)), round(f1temp(1:2))];  %dealt with matlab shit
-    xyzpair1 = vertcat(xypair1, horzcat(im1d.depth_array(xypair1(1,1), xypair1(2,1)), im2d.depth_array(xypair1(1,2), xypair1(2,2))));
-end
 
-xyzpair2 = zeros(3,2);
+    xyzpair2 = zeros(3,2);
 
-while(ismember(0, xyzpair2))
-    random2 = round(rand*length(match));
-    if random2==0
-        random2=1;
+    while(ismember(0, xyzpair2))
+        random2 = round(rand*length(match));
+        if random2==0
+            random2=1;
+        end
+        pair2 = match(:, random2);
+        %2nd random pair
+        f1temp = f1(:,pair2(1));
+        f2temp = f2(:,pair2(2));
+        xypair2 = [round(f2temp(1:2)), round(f1temp(1:2))];  %dealt with matlab shit
+
+        xyzpair2 = vertcat(xypair2, horzcat(im1d.depth_array(xypair2(2,1), xypair2(1,1)), im2d.depth_array(xypair2(2,2), xypair2(1,2))));
     end
-    pair2 = match(:, random2);
-    %2nd random pair
-    f1temp = f1(:,pair2(1));
-    f2temp = f2(:,pair2(2));
-    xypair2 = [round(f2temp(1:2)), round(f1temp(1:2))];  %dealt with matlab shit
+
+    xyzpair3 = zeros(3,2);
+
+    while(ismember(0, xyzpair3))
+        random3 = round(rand*length(match));
+        if random3==0
+            random3=1;
+        end
+        pair3 = match(:, random3);
+        %3rd random pair
+        f1temp = f1(:,pair3(1));
+        f2temp = f2(:,pair3(2));
+        xypair3 = [round(f2temp(1:2)), round(f1temp(1:2))]; %dealt with matlab shit
+
+        xyzpair3 = vertcat(xypair3, horzcat(im1d.depth_array(xypair3(2,1), xypair3(1,1)), im2d.depth_array(xypair3(2,2), xypair3(1,2))));
+    end
+
+    xyzpair4 = zeros(3,2);
+
+    while(ismember(0, xyzpair4))
+        random4 = round(rand*length(match));
+        if random4==0
+            random4=1;
+        end
+        pair4 = match(:, random4);
+        %4th random pair
+        f1temp = f1(:,pair4(1));
+        f2temp = f2(:,pair4(2));
+        xypair4 = [round(f2temp(1:2)), round(f1temp(1:2))]; %dealt with matlab shit
+
+        xyzpair4 = vertcat(xypair4, horzcat(im1d.depth_array(xypair4(2,1), xypair4(1,1)), im2d.depth_array(xypair4(2,2), xypair4(1,2))));
+    end
+
+    %Estimate transformation
+    n=4;
+    format long;
+    A = [xyzpair1(:,1), xyzpair2(:,1), xyzpair3(:,1), xyzpair4(:,1)];
+    B = [xyzpair1(:,2), xyzpair2(:,2), xyzpair3(:,2), xyzpair4(:,2)];
+
+    A_linha=A';
+    B_linha=B';
+
+
+    %solution for translation
+    T = [round(avf2x-avf1x); round(avf2y-avf1y); round(avf2z-avf1z)];  %this gives out the Translation
+
+    %plug translation
+    B_menos_T = double(B_linha) - double([T T T T]');
+    [d,Z,tr] = procrustes(double(B_menos_T), double(A_linha), 'reflection', false);
+
+    %Get xyz matches
+    xyzmatchedfeatures1=zeros(3, length(match));
+    xyzmatchedfeatures2=zeros(3, length(match));
+    for i = 1:length(match)
+        xyzmatchedfeatures1(1,i) = ceil(f1(1,match(1,i)));
+        xyzmatchedfeatures1(2,i) = ceil(f1(2,match(1,i)));
+        xyzmatchedfeatures1(3,i) = im1d.depth_array(xyzmatchedfeatures1(2,i), xyzmatchedfeatures1(1,i));
+
+
+        xyzmatchedfeatures2(1,i) = round(f2(1,match(2,i)));
+        xyzmatchedfeatures2(2,i) = round(f2(2,match(2,i)));
+        xyzmatchedfeatures2(3,i) = im2d.depth_array(xyzmatchedfeatures2(2,i), xyzmatchedfeatures2(1,i));
+    end
+
+    %Calculate im2 points from im1 points and calculated model 
+    inliers=0;
+    %B=R*A+T
+    R=tr.T;
+    B_model = double(R)*double(xyzmatchedfeatures1);
+    for i = 1:length(match)
+        B_model(:,i)=B_model(:,i)+T(:,1);
+    end
+
+    %Calculate distances between matched and calculated points
+    %Check number of inliers for that transformation
+    D=zeros(1, length(match));
+    for i = 1:length(match)
+        D(i)=norm(B_model(:,i)-xyzmatchedfeatures2(:,i));
+        if (D(i)<500)
+            inliers=inliers+1;
+        end
+    end
     
-    xyzpair2 = vertcat(xypair2, horzcat(im1d.depth_array(xypair2(1,1), xypair2(2,1)), im2d.depth_array(xypair2(1,2), xypair2(2,2))));
+    %Save the transformation with the bigger number of inliers
+    Rsave(:,:,k)=R;
+    Tsave(:,:,k)=T;
+    inlierssave(k)=inliers;
 end
 
-xyzpair3 = zeros(3,2);
+[Max, index] = max(inlierssave);
+Rfinal=Rsave(:,:,index);
+Tfinal=Tsave(:,:,index);
 
-while(ismember(0, xyzpair3))
-    random3 = round(rand*length(match));
-    if random3==0
-        random3=1;
-    end
-    pair3 = match(:, random3);
-    %3rd random pair
-    f1temp = f1(:,pair3(1));
-    f2temp = f2(:,pair3(2));
-    xypair3 = [round(f2temp(1:2)), round(f1temp(1:2))]; %dealt with matlab shit
-    
-    xyzpair3 = vertcat(xypair3, horzcat(im1d.depth_array(xypair3(1,1), xypair3(2,1)), im2d.depth_array(xypair3(1,2), xypair3(2,2))));
-end
-
-xyzpair4 = zeros(3,2);
-
-while(ismember(0, xyzpair4))
-    random4 = round(rand*length(match));
-    if random4==0
-        random4=1;
-    end
-    pair4 = match(:, random4);
-    %4th random pair
-    f1temp = f1(:,pair4(1));
-    f2temp = f2(:,pair4(2));
-    xypair4 = [round(f2temp(1:2)), round(f1temp(1:2))]; %dealt with matlab shit
-
-    xyzpair4 = vertcat(xypair4, horzcat(im1d.depth_array(xypair4(1,1), xypair4(2,1)), im2d.depth_array(xypair4(1,2), xypair4(2,2))));
-end
-
-%Estimate transformation
-n=4;
-format long;
-A = [xyzpair1(:,1), xyzpair2(:,1), xyzpair3(:,1), xyzpair4(:,1)];
-B = [xyzpair1(:,2), xyzpair2(:,2), xyzpair3(:,2), xyzpair4(:,2)];
-
-A=A';
-B=B';
-
-
-
-%solution for translation
-T = [round(avf2x-avf1x); round(avf2y-avf1y); round(avf2z-avf1z)];  %this gives out the Translation
-
-%plug translation
-B_linha = double(B) - double([T T T T]');
-[d,Z,tr] = procrustes(double(B_linha), double(A), 'reflection', false);
-
-%Check number of inliers for that transformation
-epsilon = 1;        %no idea about the scale of this error
-
-
-
-%Save the transformation with the bigger number of inliers
