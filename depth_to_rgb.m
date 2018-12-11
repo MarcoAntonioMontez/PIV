10f49905a925cb2b3d3ee0e4da9bf8d27c3db64e
TheBeggar <hugo_piku@hotmail.com>
2018-12-11 11:07:00 +0000

    depth_to_rgb with nice names

--- depth_to_rgb.m
+++ depth_to_rgb.m
@@ -1,4 +1,4 @@
-function [im_calib, im_calib_xyz, P_calib, P] = depth_to_rgb(im,depth_array)
+function [im_rgb_calib, im_rgb_vector_calib, depth_calib, P_xyz] = depth_to_rgb(im,depth_array)
 %DEPTH_TO_RGB
 % argmuments
 %   im = imread(image) 480x640x3 uint8
@@ -24,19 +24,19 @@ Z=double(depth_array(:)')/1000; %millimeters to meters
 
 % Compute correspondence between two imagens in 5 lines of code
 [v, u]=ind2sub([480 640],(1:480*640)); %compute u and v for every pixel
-P=inv(cam_params.Kdepth)*[Z.*u ;Z.*v;Z]; %depth_cam to world
-niu=cam_params.Krgb*[cam_params.R cam_params.T]*[P;ones(1,640*480)]; %depth_cam to rgb_cam
+P_xyz=inv(cam_params.Kdepth)*[Z.*u ;Z.*v;Z]; %depth_cam to world
+niu=cam_params.Krgb*[cam_params.R cam_params.T]*[P_xyz;ones(1,640*480)]; %depth_cam to rgb_cam
 u2=round(niu(1,:)./niu(3,:));
 v2=round(niu(2,:)./niu(3,:));
 
-im_calib_xyz=zeros(640*480,3);
+im_rgb_vector_calib=zeros(640*480,3);
 indsclean=find((u2>=1)&(u2<=641)&(v2>=1)&(v2<=480));
 indscolor=sub2ind([480 640],v2(indsclean),u2(indsclean));
 im1aux=reshape(im,[640*480 3]);
-im_calib_xyz(indsclean,:)=im1aux(indscolor,:); %calibrated im
+im_rgb_vector_calib(indsclean,:)=im1aux(indscolor,:); %calibrated im
 
-im_calib = uint8(reshape(im_calib_xyz,[480,640,3]));
-P_calib = uint16(reshape(P(3,:),[480,640])*1000);
+im_rgb_calib = uint8(reshape(im_rgb_vector_calib,[480,640,3]));
+depth_calib = uint16(reshape(P_xyz(3,:),[480,640])*1000);
 end
 
 % [im1_ im1_xyz P_1 P_xyz_1] = depth_to_rgb(imread('fruta2/rgb_image1_0011.png'),im1d.depth_array);
