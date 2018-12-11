a0da749b6fea68e0efcd18563fe27396a3a73b6c
TheBeggar <hugo_piku@hotmail.com>
2018-12-11 12:01:54 +0000

    updated depth_to_rgb

--- depth_to_rgb.m
+++ depth_to_rgb.m
@@ -1,17 +1,16 @@
-function [im_rgb_calib, im_rgb_vector_calib, depth_calib, P_xyz] = depth_to_rgb(im,depth_array)
+function [im_rgb_calib, im_rgb_vector_calib, P_xyz] = depth_to_rgb(im,depth_array)
 %DEPTH_TO_RGB
 % argmuments
 %   im = imread(image) 480x640x3 uint8
 %   depth_array = 480x640 uint16
 %
 % output
-%   im_calib = calibrated rbg image 
+%   im_rgb_calib = depth image calibrated in rgb image 
 %              480x640x3 (x,y,rgb) image coordinates
-%   im_calib_xyz = image vector of rgb to use in pointCloud reconstruction
+%   im_rgb_vector_calib = image vector of rgb to use in pointCloud reconstruction
 %                  307200x3 double (r,g,b)
-%   P_calib = calibrated depth.array (should be equal to depth_array)
-%             480x640 uint16 (x,y)->depth image coordinates
-%   P = image vector of depth to use in pointCloud reconstruction
+%   P_xyz = calibrated depth image (vector) in world (to use in pointCloud
+%   reconstruction)
 %       3x307200 double (x,y,z) World coordinates
 
 load cameraparametersAsus.mat
@@ -25,7 +24,7 @@ Z=double(depth_array(:)')/1000; %millimeters to meters
 % Compute correspondence between two imagens in 5 lines of code
 [v, u]=ind2sub([480 640],(1:480*640)); %compute u and v for every pixel
 P_xyz=inv(cam_params.Kdepth)*[Z.*u ;Z.*v;Z]; %depth_cam to world
-niu=cam_params.Krgb*[cam_params.R cam_params.T]*[P_xyz;ones(1,640*480)]; %depth_cam to rgb_cam
+niu=cam_params.Krgb*[cam_params.R cam_params.T]*[P_xyz;ones(1,640*480)]; %depth_cam in world to rgb_cam
 u2=round(niu(1,:)./niu(3,:));
 v2=round(niu(2,:)./niu(3,:));
 
@@ -36,7 +35,6 @@ im1aux=reshape(im,[640*480 3]);
 im_rgb_vector_calib(indsclean,:)=im1aux(indscolor,:); %calibrated im
 
 im_rgb_calib = uint8(reshape(im_rgb_vector_calib,[480,640,3]));
-depth_calib = uint16(reshape(P_xyz(3,:),[480,640])*1000);
 end
 
 % [im1_ im1_xyz P_1 P_xyz_1] = depth_to_rgb(imread('fruta2/rgb_image1_0011.png'),im1d.depth_array);
